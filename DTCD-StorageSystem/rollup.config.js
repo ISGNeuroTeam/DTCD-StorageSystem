5517dc733082f370848b181009eb18f7b6afa497
rkuramshin <rkuramshin@isgneuro.com>
2022-02-10 15:37:36 +0300

    addes version of systems in adapters

--- DTCD-StorageSystem/rollup.config.js
+++ DTCD-StorageSystem/rollup.config.js
@@ -4,23 +4,30 @@ import alias from '@rollup/plugin-alias';
 import path from 'path';
 import pluginMeta from './src/Plugin.Meta';
 
+import json from '@rollup/plugin-json';
+
+import { version } from './package.json';
+
 const watch = Boolean(process.env.ROLLUP_WATCH);
 
 const pluginName = pluginMeta.name;
 const outputFile = `${pluginName}.js`;
-const outputDirectory = watch ? `./../../DTCD/server/plugins/DTCD-${pluginName}` : `./build`;
+const outputDirectory = watch
+  ? `./../../DTCD/server/plugins/DTCD-${pluginName}_${version}`
+  : `./build`;
 
 const plugins = [
   nodeResolve(),
   alias({
     entries: {
       '@': path.resolve(__dirname, 'src'),
-      'SDK': path.resolve(__dirname, './../DTCD-SDK'),
-    }
+      SDK: path.resolve(__dirname, './../DTCD-SDK'),
+    },
   }),
   babel({
     babelHelpers: 'bundled',
   }),
+  json(),
 ];
 
 export default {
